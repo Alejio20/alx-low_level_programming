a1a470fbe85b10c1c2d0585665a2538ce3bd3fd5
Alejio20 <humbleotamiri@gmail.com>
2022-12-08 09:47:18 -0800

    Added size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char Char;
+int Int;
+long int Long_Int;
+long long int Long_Long_Int;
+float Float;
+
+printf("Size of a char: %zu byte(s)\n",sizeof(Char));
+printf("Size of an int: %zu byte(s)\n",sizeof(Int));
+printf("Size of a long int: %zu byte(s)\n",sizeof(Long_Int));
+printf("Size of a long long int: %zu byte(s)\n",sizeof(Long_Long_Int));
+printf("Size of a float: %zu byte(s)\n",sizeof(Float));
+
+return (0);
+
+}
--- 0x00-hello_world/README.md
+++ 0x00-hello_world/README.md
@@ -4,3 +4,4 @@
 3-name: Script Compiles a C file and creates an executable named cisfun
 4-puts.c: Prints exactly: "Programming is like building a multilingual puzzle, followed by a new line using puts function
 5-printf.c: Prints exactly: with proper grammar, but the outcome is a piece of art, followed by a new line using printf function
+6-size.c: Prints the size of various types on the computer it is compiles and run on.
