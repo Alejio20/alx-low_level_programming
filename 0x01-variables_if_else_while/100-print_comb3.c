46ca315c3e19e4e0473837b2b9f42f8e06387a95
Alejio20 <humbleotamiri@gmail.com>
2022-12-09 14:42:13 -0800

    Added print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -28,8 +28,12 @@ int main(void)
 			{
 				putchar(i);
 				putchar(n);
-				putchar(',');
-				putchar(' ');
+				
+				if (i != 56 && n != 57)
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
 		}
 
