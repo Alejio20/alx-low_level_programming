699bbc3f4f9bfc3128e9d4c8e923168165e49d8f
Alejio20 <humbleotamiri@gmail.com>
2022-12-09 13:39:13 -0800

    Added print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible different combinations of two digits
+ * separated by comma followed by a space. The two digits must be different
+ * Only print the smallest combination of two digits, if the two combinations
+ * have the same digits. Also print in ascending order.
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	for (int i = 48; i <= 57; i++)
+	{
+		for (int n = 48; n <= 57; n++)
+		{
+			if (i == n)
+			{
+				continue;
+			}
+			else
+			{
+				putchar(i);
+				putchar(n);
+
+				if (n < 57)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+
+		if (i < 57)
+		{
+			putchar(',');
+		}
+	}
+}
