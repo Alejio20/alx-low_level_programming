ab03534d7c247691e22a299ed429d1e72ee324f1
Alejio20 <humbleotamiri@gmail.com>
2022-12-13 20:45:45 -0800

    Added 24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minjte of the day of Jack Bauer
+ * starting from 00:00 to 23:59
+ * Return: Always 0
+ */
+
+void jack_bauer(void)
+{
+	int i, n;
+
+	i = 0;
+	while (i < 24)
+	{
+		n = 60;
+		while (n < 60)
+		{
+			_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
+			_putchar(':');
+			_putchar((n / 10) + '0');
+			_putchar((n % 10) + '0');
+			_putchar('\n');
+
+			n++;
+		}
+
+		i++;
+	}
+}
