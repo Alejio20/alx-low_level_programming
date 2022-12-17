b759fed79f3673ff12d41d9d4fabaaddd59d50ba
Alejio20 <humbleotamiri@gmail.com>
2022-12-16 21:43:52 -0800

    Added print_remaining_days.c

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -12,6 +12,7 @@
 
 void print_remaining_days(int month, int day, int year)
 {
+    /**leap years are divisible by 100 and 400 or by 4*/
     if ((year % 100 == 0 && year % 400 == 0) || (year % 4 == 0))
     {
         if (month > 2 && day >= 60)
