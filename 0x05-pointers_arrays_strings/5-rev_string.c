0745e45c4d373536bccd5958ff659b4abe4d0cc7
Alejio20 <humbleotamiri@gmail.com>
2022-12-19 13:40:11 -0800

    Added rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ * @s: char pointer
+ * Return: void
+ */
+
+void rev_string(char *s)
+{
+	int i, j;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	for (j = i - 1; j >= 0; j--)
+	{
+		_putchar(s[j]);
+	}
+	_putchar('\n');
+}
