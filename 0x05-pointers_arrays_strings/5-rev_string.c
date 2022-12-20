d310f0e7095f31734bd39aae05339f5ebe8236e3
Alejio20 <humbleotamiri@gmail.com>
2022-12-19 20:38:32 -0800

    rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,16 +8,19 @@
 
 void rev_string(char *s)
 {
-	int i, j;
+	int l, i;
+	char ch;
 
-	i = 0;
-	while (s[i] != '\0')
+	/*find string length without null char*/
+	for (l = 0; s[l] != '\0'; ++l)
+		;
+
+	/*swap the string by looping to half the string*/
+	for (i = 0; i < l / 2; ++i)
 	{
-		i++;
+		ch = s[i];
+		s[i] = s[l - 1 - i]; /*-1 because the array starts from 0*/
+		s[l - 1 - i] = ch;
 	}
 
-	for (j = i - 1; j >= 0; j--)
-	{
-		*s = s[j];
-	}
 }
