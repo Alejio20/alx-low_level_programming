7bd93d29dc5c1315e53ff0225a9785259d8d04b9
Alejio20 <humbleotamiri@gmail.com>
2022-12-19 18:37:13 -0800

    Added puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -12,16 +12,16 @@ void puts2(char *str)
 	int i;
 
 	i = 0;
-	while (*(str + i) != '\0')
+	while (str[i] != '\0')
 	{
 		if (i == 0)
 		{
-			_putchar(*str);
+			_putchar(str[i]);
 			i++;
 		}
 		else
 		{
-			_putchar(*(str + (i + 1)));
+			_putchar(str[i + 1]);
 			i++;
 		}
 	}
