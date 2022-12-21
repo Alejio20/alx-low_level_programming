5531afe2ce1808ff76ce5def4980cba841524913
Alejio20 <humbleotamiri@gmail.com>
2022-12-20 19:54:15 -0800

    Added atoi.c

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _atoi - convert a string to an integer
+ * @s: char pointer
+ * Returns: int of converted string
+ */
+
+int _atoi(char *s)
+{
+	int i, n, sign;
+
+	n = 0;
+	sign = 1;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] >= 48 && s[i] <= 57)
+		{
+			n *= 10 + (s[i] - 48);
+		}
+		else if (s[i] == 45)
+		{
+			sign *= -1;
+		}
+		i++;
+	}
+
+	return (sign * n);
+}
