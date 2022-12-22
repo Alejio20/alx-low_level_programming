066bea65340aa185d4d3eaa74640d1deef069d5d
Alejio20 <humbleotamiri@gmail.com>
2022-12-22 12:57:08 -0800

    Added strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -16,7 +16,9 @@ char *_strncpy(char *dest, char *src, int n)
 	{
 		dest[i] = src[i];
 	}
-
+	
+	// write additional null bytes to dest
+	// to ensure total n bytes are written, if src < n
 	while (i < n)
 	{
 		dest[i] = '\0';
