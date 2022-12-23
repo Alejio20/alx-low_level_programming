09ba6470a0721af7ea0085acdfb4f17bb5cb7f9f
Alejio20 <humbleotamiri@gmail.com>
2022-12-22 16:42:39 -0800

    Added cap_string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes all words of a string
+ * @s: string pointer
+ * Return: capitalized string
+ */
+
+char *cap_string(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i] = '\0')
+	{
+		if (s[i] == 20 || s[i] == 21 || s[i] == 44 || s[i] == 46 || s[i] == 58 || s[i] == 59 || s[i] = 63 || s[i] == 28 || s[i] == 29 || s[i] == 22 || s[i] == 123 || s[i] == 125 || s[i] == 9)
+		{
+			if (*s(i + 1) > 96 && *s(i + 1) < 123)
+			{
+				*s(i + 1) -= 32;
+			}
+		}
+		else
+		{
+			if (s[i] > 96 && s[i] < 123)
+			{
+				s[i] -= 32;
+			}
+		}
+		i++;
+	}
+
+	return (s);
+}
