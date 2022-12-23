38fb08366e67fbd803ff50322884d06119c39d00
Alejio20 <humbleotamiri@gmail.com>
2022-12-22 20:58:10 -0800

    Added leet

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * leet - encodes a string into 1337
+ * letter a and A should be replaced by 4
+ * letter e and E should be replced by 3
+ * letter o and O should be replaced by 0
+ * letter t and T should be replaced by 7
+ * letter l and L should  be replaced by 1
+ * Return: encoded string
+ */
+
+char *leet(char *s)
+{
+	int i, j;
+	int x[] = {65, 69, 79, 84, 76}
+	int y[] = {97, 101, 111, 116, 108}
+	int z[] = {52, 51, 48, 55, 49}
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		for (j = 0; j < 5; j++)
+		{
+			if (s[i] == x[j] || s[i] == z[j])
+			{
+				s[i] = z[j];
+				break;
+			}
+		}
+		i++;
+	}
+	return (s);
+}
