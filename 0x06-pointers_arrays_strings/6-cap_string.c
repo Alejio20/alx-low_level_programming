a5c9e1811daca4625f3d5a4d70e43c9cca5e089a
Alejio20 <humbleotamiri@gmail.com>
2022-12-23 19:58:24 -0800

    Added cap_string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,24 +8,36 @@
 
 char *cap_string(char *s)
 {
-	int i;
+	int i = 0;
 
-	i = 0;
-	while (s[i] = '\0')
+	/*iterate through our array values*/
+	while (s[i] != '\0')
 	{
-		if (s[i] > 96 && s[i] < 123)
+		/*check for any lowercase letters*/
+		if (s[i] >= 97 && s[i] <= 122)
 		{
+			/**
+			 * if we have a null character
+			 * change its value to capital
+			*/
 			if (i == 0)
 			{
 				s[i] -= 32;
 			}
-			if (s[i - 1] == 32 || s[i - 1] == 9 || s[i - 1] == 10 || s[i - 1] == 44 || s[i - 1] == 59 || s[i - 1] == 46 || s[i - 1] == 33 || s[i - 1] == 63 || s[i - 1] == 34 || s[i - 1] == 40 || s[i - 1] == 41 || s[i - 1] == 123 || s[i - 1] == 125)
+			/**
+			 * if we find any character matching the below before any small
+			 * letter we change that value to a capital letter.
+			*/
+			if (s[i - 1] == 32 || s[i - 1] == 9 || s[i - 1] == 10 ||
+				s[i - 1] == 44 || s[i - 1] == 59 || s[i - 1] == 46 ||
+				s[i - 1] == 33 || s[i - 1] == 63 || s[i - 1] == 34 ||
+				s[i - 1] == 40 || s[i - 1] == 41 || s[i - 1] == 123 ||
+				s[i - 1] == 124)
 			{
 				s[i] -= 32;
 			}
 		}
 		i++;
 	}
-
 	return (s);
 }
