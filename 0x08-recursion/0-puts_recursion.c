2cce7ec194760205f7d01d71f2af9bec8b5285fc
Alejio20 <humbleotamiri@gmail.com>
2023-01-04 17:49:03 -0800

    Added puts_recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,11 +8,11 @@
 
 void _puts_recursion(char *s)
 {
-	unsigned int i = 0;
 
-	if (s[i] == '\0')
-		return;
-	_putchar(s[i]);
-	i++;
-	_puts_recursion(s);
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	_putchar('\n');
 }
