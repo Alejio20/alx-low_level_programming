3004b0e60551ec85a7d20257488133bd64807849
Alejio20 <humbleotamiri@gmail.com>
2023-01-07 10:10:55 -0800

    Added args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints the number of arguments passed into it
+ * @argc: argument count
+ * @argv: string array of arguments
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%i\n", argc);
+	return (0);
+}
