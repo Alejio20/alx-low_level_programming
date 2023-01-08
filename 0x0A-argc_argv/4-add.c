7dba0e04b069e77c1cc615b3d951cfb6aa3a41d8
Alejio20 <humbleotamiri@gmail.com>
2023-01-07 20:51:06 -0800

    Added add

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -13,6 +13,8 @@ int main(int argc, char *argv[])
 {
 	int i, j, sum;
 
+	sum = 0;
+
 	for (i = 1; i < argc; i++)
 	{
 		for (j = 0; argv[i][j] != '\0'; j++)
@@ -23,8 +25,8 @@ int main(int argc, char *argv[])
 				return (1);
 			}
 		}
-		add += atoi(argv[i]);
+		sum += atoi(argv[i]);
 	}
-	printf("%d\n", add);
+	printf("%d\n", sum);
 	return (0);
 }
