9dd9fa6bab1c6f0a986db594e027bf232b612cb0
Alejio20 <humbleotamiri@gmail.com>
2023-01-07 22:25:14 -0800

    Added change

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -21,27 +21,20 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		return (1);
 	}
-	
 	if (cents < 0)
 	{
 		printf("0\n");
 	}
-
 	quarters = cents / 25;
 	cents -= quarters * 25;
-
 	dimes = cents / 10;
 	cents -= dimes * 10;
-
 	nickels = cents / 5;
 	cents -= nickels * 5;
-
 	longacres = cents / 2;
 	cents -= longacres * 2;
-
 	pennies = cents / 1;
 	cents -= pennies * 1;
-
 	change = quarters + dimes + nickels + longacres + pennies;
 	printf("%d\n", change);
 	return (0);
