df6ddae5d99522d468321058db4f7969fc808c03
Alejio20 <humbleotamiri@gmail.com>
2023-01-12 01:10:17 -0800

    Added str_concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,44 +11,41 @@
 
 char *str_concat(char *s1, char *s2)
 {
-	int i, j, k, x, y;
-	char *concat;
+	char *s3;
+	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
 
-	x = 0;
-	while (s1[x] != '\0')
-		x++;
-	y = 0;
-	while (s2[y] != '\0')
-		y++;
+	while (s1 && s1[len1])
+		len1++;
+	while (s2 && s2[len2])
+		len2++;
 
-	concat = malloc(sizeof(char) * (x + y + 1));
-	if (concat == NULL)
+	s3 = malloc(sizeof(char) * (len1 + len2 + 1));
+	if (s3 == NULL)
 		return (NULL);
+
+	i = 0;
+	j = 0;
 
 	if (s1)
 	{
-	i = 0;
-	j = 0;
-	while (s1[j] != '\0')
-	{
-		concat[i] = s1[j];
-		i++;
-		j++;
+		while (i < len1)
+		{
+			s3[i] = s1[i];
+			i++;
+		}
 	}
-	}
+
 	if (s2)
 	{
-	k = 0;
-	while (s2[k] != '\0')
-	{
-		concat[i] = s2[k];
-		i++;
-		k++;
+		while (i < (len1 + len2))
+		{
+			s3[i] = s2[j];
+			i++;
+			j++;
+		}
 	}
-	}
+	s3[i] = '\0';
 
-	concat[i] = '\0';
-
-	return (concat);
-	free(concat);
+	return (s3);
 }
+
