c95e188749a67502137c57289be558d627aa448e
Alejio20 <humbleotamiri@gmail.com>
2023-01-13 04:22:56 -0800

    Added wildcmp

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * compares two strings and returns 1 if the strings
+ * wildcmp - compares two strings and returns 1 if the strings
  * can be considered identical, otherwise return 0
  * @s1: first string
  * @s2: second string which can contain * as a special character
