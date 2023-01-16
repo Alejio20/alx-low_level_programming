e522eb4093bda74c379705f58537d27a9137ef4e
Alejio20 <humbleotamiri@gmail.com>
2023-01-16 09:34:55 -0800

    Added main

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
 #include <string.h>
 
-/*#define __FILENAME__ ((strrchr(__FILE__, '/')) ? (strrchr(__FILE__, '/') + 1) : (__FILE__))*/
+#define __FILENAME__ ((strrchr(__FILE__, '/')) ? (strrchr(__FILE__, '/') + 1) : (__FILE__))
 
 /**
  * main - prints the name of the file it was compiled from
  * followed by a new line
- * @void
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	printf("%s\n",__FILE__);
+	printf("%s\n",__FILENAME__);
 	return (0);
 }
