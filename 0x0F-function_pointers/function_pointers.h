c9d7e41ac1271c32cc09d7787a26af6a6d6a438b
Alejio20 <humbleotamiri@gmail.com>
2023-01-18 14:10:06 -0800

    Added array_iterator

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,8 @@
 #ifndef _FUNCTION_POINTERS_H_
 #define _FUNCTION_POINTERS_H_
 
+typedef unsigned int size_t;
+
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 
