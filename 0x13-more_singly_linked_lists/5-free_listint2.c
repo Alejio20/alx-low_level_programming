31dc545b072c0dff2cae42c688157788090ee5b8
Alejio20 <humbleotamiri@gmail.com>
2023-01-31 08:14:58 -0800

    Added free_listint2

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,13 +10,16 @@ void free_listint2(listint_t **head)
 {
 	listint_t *ptr;
 
-	ptr = *head;
-
-	while (ptr != NULL)
+	if (head != NULL)
 	{
-		*head = *head->next;
-		free(ptr);
 		ptr = *head;
+
+		while (ptr != NULL)
+		{
+			*head = *head->next;
+			free(ptr);
+			ptr = *head;
+		}
+		*head = NULL;
 	}
-	*head = NULL;
 }
