3fa280b8baeb96dc15a2ad5637b579662fdac10e
Alejio20 <humbleotamiri@gmail.com>
2023-01-31 12:48:46 -0800

    Added get_nodeint

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -11,13 +11,8 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int i;
 
-	if (head == NULL)
-		return (NULL);
-	else
-	{
-		for (i = 0; i < index; i++)
-			head = head->next;
-	}
+	for (i = 0; i < index && head != NULL; i++)
+		head = head->next;
 
 	return (head);
 }
