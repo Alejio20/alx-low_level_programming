e4785aa3a9850069c1b54f7af4595b70e0f3e66b
Alejio20 <humbleotamiri@gmail.com>
2023-01-31 06:58:28 -0800

    Added add_nodeint_end

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -22,11 +22,12 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 
 	if (ptr == NULL)
 		*head = tmp;
+	else
+	{
+		while (ptr->next != NULL)
+			ptr = ptr->next;
 
-	while (ptr->next != NULL)
-		ptr = ptr->next;
-
-	ptr->next = tmp;
-
+		ptr->next = tmp;
+	}
 	return (*head);
 }
