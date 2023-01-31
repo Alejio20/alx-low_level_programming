35d4d716f9dc77d1bb703d3c2a02e115ca1296aa
Alejio20 <humbleotamiri@gmail.com>
2023-01-30 16:52:59 -0800

    Added add_nodeint

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -16,12 +16,12 @@ listint_t *add_nodeint(listint_t **head, const int n)
 		return (NULL);
 
 	if (head == NULL)
-		ptr->next = NULL;
+		return (NULL);
 
 	ptr->n = n;
 	ptr->next = *head;
 
 	*head = ptr;
 
-	return (head);
+	return (*head);
 }
